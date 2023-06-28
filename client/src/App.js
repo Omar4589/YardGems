c13170ac34d972314717032e8dae3d381978c8e7
Rolyat512 <122414452+Rolyat512@users.noreply.github.com>
2023-06-28 17:34:10 -0500

    added .env file

---
+++ client/src/App.js
@@ -0,0 +1,45 @@
+import React from "react";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem("id_token");
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div>Enter your code here</div>
+      <Router>
+        <Routes>
+          <Route path="/" />
+        </Routes>
+      </Router>
+    </ApolloProvider>
+  );
+}
+
+export default App;
