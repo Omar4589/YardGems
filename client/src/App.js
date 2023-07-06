39ffc016abd1809d7cb3954ef223cdefc9cd5906
Omar4589 <omar4589@hotmail.com>
2023-07-05 19:44:29 -0500

    imports GoogleMaps component to Home.js and adds light styling

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,6 @@ import {
 } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import GoogleMaps from "../src/components/googleMaps/GoogleMaps";
 import MyListings from "./pages/MyListings";
 import SignUpLoginPage from "./pages/SignUpLogin";
 import Test from "./pages/HeaderTest";
@@ -16,6 +15,7 @@ import Footer from "./components/Footer/Footer";
 import AboutUs from "./pages/AboutUs";
 import SinglePost from "./pages/SinglePost";
 import BottomNavBar from "./components/Nav/nav";
+import Home from "./pages/Home/Home";
 
 const httpLink = createHttpLink({
   uri: "/graphql",
@@ -45,7 +45,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/MyListings" element={<MyListings />} />
-          <Route path="/" element={<GoogleMaps />} />
+          <Route path="/" element={<Home />} />
           <Route path="/test" element={<Test />} />
           <Route path="/signup-login" element={<SignUpLoginPage />} />
           <Route path="/AboutUs" element={<AboutUs />} />
---
+++ client/src/pages/Home/styles.js
@@ -0,0 +1,15 @@
+const styles = {
+  mainContainer: {
+    display: "flex",
+    justifyContent: "center",
+    width:"100%"
+  },
+  map: {
+    width: "50%",
+  },
+  listings: {
+    width: "50%",
+  },
+};
+
+export default styles;
