267c4cc68df01e17ccb797b3b0414ef54d0ad42a
Rolyat512 <122414452+Rolyat512@users.noreply.github.com>
2023-07-06 19:14:00 -0500

    changed nav showLabels

--- client/src/components/Nav/nav.js
+++ client/src/components/Nav/nav.js
@@ -62,7 +62,7 @@ export default function BottomNavBar() {
 				<BottomNavigation
 					value={value}
 					onChange={handleChange}
-					// showLabels='true'
+					showLabels='true'
 				>
 					<BottomNavigationAction
 						component={ RouterLink }
