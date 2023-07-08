6e8944d9fd6abf0581fd63b128a404c83a6b235e
Omar4589 <omar4589@hotmail.com>
2023-07-08 00:19:26 -0500

    updates styling of sign up form for mobile view

---
+++ client/src/components/SignUpForm/styles.js
@@ -0,0 +1,33 @@
+const styles = {
+  mainContainer: {
+    background: "#e8f5e9",
+    height: "100vh",
+    display: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingBottom: 10,
+  },
+  signupContainer: {
+    background: "white",
+    mx: 3,
+    px: 4,
+    borderRadius: 2,
+  },
+  appName: { py: 2, px: 2 },
+  heading: { fontWeight: "bold", fontSize: 25, mb: 1 },
+  form: {},
+  inputBoxes: { py: 1 },
+  labels: { fontWeight: "semibold", mb: 1, display: "block" },
+  button: { bgcolor: "#66bb6a", color: "white", fontWeight: "semibold", py: 1 },
+  login: { my: 3, textAlign: "center" },
+  loginButton: {
+    color: "#66bb6a",
+    margin: 10,
+    p: 1,
+    "&:hover": {
+      color: "#66bb6a",
+    },
+  },
+};
+
+export default styles;
