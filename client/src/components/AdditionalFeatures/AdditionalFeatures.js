c02f5a8733d38847cd1d528dc819a85f4c408109
Omar4589 <omar4589@hotmail.com>
2023-07-09 18:12:08 -0500

    creates additionaFeatures component that shows up when youre not logged in and try to access 'my listings'

---
+++ client/src/components/AdditionalFeatures/AdditionalFeatures.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import { Box, Typography } from "@mui/material";
+
+const AdditionalFeatures = () => {
+  return (
+    <Box sx={{ textAlign: "center" }}>
+      <Typography>
+        In order to access additional features, such as creating a sale listing
+        and saving a sale listing, you need to create an account and login.
+      </Typography>
+    </Box>
+  );
+};
+
+export default AdditionalFeatures;
---
+++ client/src/pages/MyListings/styles.js
@@ -0,0 +1,5 @@
+const styles = {
+  mainContainer: {},
+};
+
+export default styles;
