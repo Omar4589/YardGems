7ed40f3439dd81dfae4f0bc66617c7ef67f7b2f5
Omar4589 <omar4589@hotmail.com>
2023-07-09 21:09:18 -0500

    updates style for desktop view in AboutUs page

--- client/src/pages/AboutUs/styles.js
+++ client/src/pages/AboutUs/styles.js
@@ -1,5 +1,11 @@
 const styles = {
-  mainContainer: { mx: "auto", px: 3, py: 5, backgroundColor: "#e8f5e9" },
+  mainContainer: {
+    mx: "auto",
+    px: 3,
+    py: 5,
+    backgroundColor: "#e8f5e9",
+    "@media (min-width: 768px)": { padding: "5% 8% 5% 8%" },
+  },
   heading: { mb: 3, fontSize: 35 },
   statement: { mb: 4, fontSize: 20 },
   lastStatment: { mb: 12, fontSize: 20 },
