6e8d0c83cf5234586c73b8d00948e1570732c0d5
Omar4589 <omar4589@hotmail.com>
2023-07-10 21:26:57 -0500

    destructures styles from modalStyles

--- client/src/components/DashboardModal/DashboardModal.js
+++ client/src/components/DashboardModal/DashboardModal.js
@@ -7,7 +7,7 @@ import Typography from '@mui/material/Typography';
 import { ADD_POST,  } from '../../utils/mutations';
 import { TextField, Container} from '@mui/material';
 import {useMutation, } from '@apollo/client';
-import style from './modalStyles';
+import {style} from './modalStyles';
 import usePlacesAutocomplete, { getGeocode, getLatLng } from 'use-places-autocomplete';
 import { Combobox, ComboboxInput, ComboboxPopover, ComboboxList, ComboboxOption } from '@reach/combobox';
 import '@reach/combobox/styles.css';
