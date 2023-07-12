e2771b44fc414b7f51fa39eb62ae04b80943fb5a
Rolyat512 <122414452+Rolyat512@users.noreply.github.com>
2023-07-11 21:33:24 -0500

    testing secret

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,7 +1,8 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-const secret = process.env.REACT_APP_SECRET_KEY
+// const secret = process.env.REACT_APP_SECRET_KEY
+const secret = 'testingOneTwoThree'
 const expiration = "5h";
 
 module.exports = {
