c888793db1aaf1a6840e2a908bd3c1885e146800
Omar4589 <omar4589@hotmail.com>
2023-08-07 20:08:23 -0500

    adds listingModal to AllListingsComponent and loginPopOver

--- client/src/components/AllListings/AllListingsComponent.js
+++ client/src/components/AllListings/AllListingsComponent.js
@@ -10,6 +10,8 @@ import {
   IconButton,
   Popover,
   CardActionArea,
+  Modal,
+  Box,
 } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
 import image from "../../assets/yardsale.jpg"; // hard coding for now
@@ -24,6 +26,25 @@ export default function AllListings() {
   //it is the listings that we query using GraphQL and the loggedInUser's info.
   const { listings, setListings } = useListingContext();
 
+  //---------STATES--------//
+
+  //State for 'Please login' pop over
+  //populates when a user clicks on a listing to favorite it, but theyre not logged in
+  const [loginPopOver, setLoginPopOver] = useState(false);
+
+  //State for card modal
+  const [listingModal, setListingModal] = useState(null);
+
+  //-----MODAL HANDLERS------//
+
+  //Opens modal when listings is clicked on
+  const openModal = (listing) => setListingModal(listing);
+  //Closes modal when listings is clicked on
+  const closeModal = () => setListingModal(false);
+
+  //Closes pop over message - 'Please log in'
+  const closePopOver = () => setLoginPopOver(false);
+
   //---------------------------RETURN STATEMENT-------------------------//
   return (
     <Container sx={styles.container}>
@@ -31,7 +52,7 @@ export default function AllListings() {
         {listings.map((listing) => {
           return (
             <Card component="div" sx={{}}>
-              <CardActionArea>
+              <CardActionArea onClick={() => openModal(listing)}>
                 <CardHeader
                   title={listing.title}
                   subheader={`Listed by: ${listing.author}`}
@@ -54,11 +75,17 @@ export default function AllListings() {
                   <FavoriteIcon />
                 </IconButton>
               ) : (
-                <IconButton sx={styles.iconButton} aria-label="favorite">
+                <IconButton
+                  onClick={() => setLoginPopOver(true)}
+                  sx={styles.iconButton}
+                  aria-label="favorite"
+                >
                   <FavoriteIcon sx={{ color: "grey" }} />
                 </IconButton>
               )}
               <Popover
+                open={loginPopOver}
+                onClose={closePopOver}
                 anchorOrigin={{
                   vertical: "bottom",
                   horizontal: "left",
@@ -72,6 +99,50 @@ export default function AllListings() {
           );
         })}
       </Grid>
+
+      {/* MODAL BELOW */}
+      {listingModal && (
+        <Modal
+          open={Boolean(listingModal)}
+          onClose={closeModal}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={styles.modalPopUp}>
+            <Card component="div" sx={{ maxWidth: "100%" }}>
+              <CardHeader
+                title={listingModal.title}
+                subheader={`Post By: ${listingModal.author}`}
+              />
+              <CardMedia
+                sx={{ height: 140, paddingTop: "56.2%" }}
+                image={image}
+              />
+              <CardContent component="div">
+                <Typography component="span" gutterBottom variant="h5">
+                  Date Of Event: {listingModal.dateOfSale}
+                </Typography>
+                <Typography
+                  component="div"
+                  variant="body2"
+                  color="text.secondary"
+                >
+                  {listingModal.description}
+                  <Typography
+                    component="div"
+                    variant="body2"
+                    color="text.secondary"
+                  >
+                    <br></br>
+                    {listingModal.address}
+                  </Typography>
+                </Typography>
+              </CardContent>
+            </Card>
+          </Box>
+        </Modal>
+      )}
+      {/* END OF MODAL */}
     </Container>
   );
 }
