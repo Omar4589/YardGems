75cb432210942ad85249a4992d3906b843544952
Omar4589 <omar4589@hotmail.com>
2023-08-12 00:31:59 -0500

    adds comments to ListingContext.js

--- client/src/utils/ListingContext.js
+++ client/src/utils/ListingContext.js
@@ -6,30 +6,29 @@ import { QUERY_LISTINGS, ME_QUERY } from "./queries";
 //Here we create the context
 const ListingContext = createContext();
 
-// Custom hook to provide usage of the listing context
+// Here we create a  hook to provide usage of the listing context, we use 'export' to make it exportable
 export const useListingContext = () => useContext(ListingContext);
 
 //----------------------------START OF CONTEXT PROVIDER--------------------//
 export const ListingProvider = ({ children }) => {
   //-----------------STATE-------------------//
+  //Here we create a state called 'listings' that we will use to keep track of ALL LISTINGS in database
+  //We first set it to an empty array because we havent fetched the data yet
   const [listings, setListings] = useState([]);
 
   //-----------------QUERIES-----------------//
-
   const { data: allListingsData } = useQuery(QUERY_LISTINGS);
 
   const { data: loggedInUserData } = useQuery(ME_QUERY);
-  //Here we set the user data to a variable
-  const loggedInUser = loggedInUserData?.me || [];
 
   //-----------------HOOKS-------------------//
-
-  //This useEffect hook runs when component mounts and sets listings state to initial listings query
+  //The useEffect hook below runs when component mounts and sets the listings state to the allListingsData
   // This useEffect hook updates the listings state to render any
   // new listings that are added, we set allListingsData as a dependency so that this hook runs when there is a change to allListingsData
 
   useEffect(() => {
     // Variable that holds all listings, an array of objects containing listings props
+    //if there are no listings available, we set this variable to an empty array
     const allListings = allListingsData?.allListings || [];
 
     setListings(allListings);
@@ -38,7 +37,7 @@ export const ListingProvider = ({ children }) => {
   //---------------RETURN STATEMENT-------------------//
 
   // The value prop expects an initial state object, in this case
-  //the initial state is the listings state, we also pass in the loggedInUser's info
+  //the initial state is the listings state, we also pass in the state setter and loggedInUser's info in case we need it
   return (
     <ListingContext.Provider value={{ listings, setListings, loggedInUser }}>
       {children}
