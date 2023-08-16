267d982eed8ae885c16dc1790a155fcf28c1334b
Omar4589 <omar4589@hotmail.com>
2023-08-16 02:10:14 -0500

    adds comments to ContactUs.js

--- client/src/pages/ContactUs/ContactUs.js
+++ client/src/pages/ContactUs/ContactUs.js
@@ -1,10 +1,12 @@
+//-----------------IMPORTS-----------------------//
 import { Box, Button, TextField, Typography } from "@mui/material";
 import React, { useState } from "react";
 import styles from "./styles";
-import emailjs from "@emailjs/browser";
+import emailjs from "@emailjs/browser"; //For more info visit : https://www.emailjs.com/docs/
 
-// Component for the contact us form
+//-----------------------START OF COMPONENT-----------------------//
 const ContactUs = () => {
+  //-----------------STATE---------------//
   // State to store form data
   const [formData, setFormData] = useState({
     user_name: "",
@@ -12,7 +14,9 @@ const ContactUs = () => {
     message: "",
   });
 
-  // Event handler for input field changes
+  //--------------FORM FIELD HANDLERES-----------//
+  // Event handler for input field changes, this updates the state of the input fields to
+  //match what the user is typing
   const handleInputChange = (event) => {
     const { name, value } = event.target;
 
@@ -24,13 +28,9 @@ const ContactUs = () => {
   };
 
   // Event handler for form submission
+  //This function is responsible for submitting the contact us form and invoking the sendEmail function
   const handleFormSubmit = async (event) => {
     event.preventDefault();
-
-    // Log form data to the console
-    console.log(formData.user_name);
-    console.log(formData.user_email);
-    console.log(formData.message);
 
     try {
       await sendEmail();
@@ -38,26 +38,36 @@ const ContactUs = () => {
       console.error(err);
     }
   };
+
+  //When using EMAILJS, we must first initiate the service using init() which takes a PUBLIC_KEY as a parameter
+  //The public key is provided by EMAILJS when you sign up .
+  // https://dashboard.emailjs.com/admin/account
   emailjs.init("yHQDycNvnINCMJg1d");
 
+  //This function uses emailjs's sendForm method to send the actual email
   const sendEmail = (event) => {
     try {
+      //Get a handle on the HTML form element
       const form = document.getElementById("contactus-form");
-      // these IDs from the previous steps
+
+      //sendForm() takes in 3 parameters,
+      //1: the service ID of your email service; this is provided when you 'create a service' in the EMAILJS dashboard;
+      //I.E. if youre using a gmail email, you 'create a service' for GMAIL and enter your gmail details. EMAILJS will the provide you with an ID
+      //2. the Template ID of the email template you want to use. You can create email templates, visit https://www.emailjs.com/docs/tutorial/creating-email-template/
+      //3. The HTML form element; above we get a handle on it using getElementById method
       emailjs.sendForm("service_y0kbg4u", "contactus_form", form);
-      // Clear input fields
+
       // Clear input fields visually
       document.getElementById("user_name").value = "";
       document.getElementById("user_email").value = "";
       document.getElementById("message").value = "";
-      console.log(
-        "Email Sent Successfully and input fields have been cleared!"
-      );
+      console.log("Email Sent Successfully!");
     } catch (err) {
       console.log(err);
     }
   };
 
+  //----------------------RETURN STATEMENT------------------------//
   // Render the contact form component
 
   return (
