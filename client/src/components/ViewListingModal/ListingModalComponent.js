a969927de8b134b8ee5909bd5704a54040d444b0
Omar4589 <omar4589@hotmail.com>
2023-10-02 20:47:01 -0500

    adds closing buttom to view listing modal and updates style for better responsiveness:
    /

--- client/src/components/ViewListingModal/ListingModalComponent.js
+++ client/src/components/ViewListingModal/ListingModalComponent.js
@@ -9,11 +9,10 @@ import {
   Modal,
   Box,
 } from "@mui/material";
-import  styles  from "./styles";
+import styles from "./styles";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
 
 // settings for react-slick's Slider component
 const settings = {
@@ -34,41 +33,72 @@ export default function ListingModalComponent({
 }) {
   return (
     <Modal
-    id="listing-modal-container"
+      id="listing-modal-container"
       open={Boolean(listingModal)}
       onClose={closeModal}
       aria-labelledby="modal-modal-title"
       aria-describedby="modal-modal-description"
     >
-      <Box id="listing-modal"  
-      sx={{...styles.modalPopUp}}>
-        <Card component="div" sx={{ maxWidth: "100%" }}>
+      <Box id="listing-modal" sx={{ ...styles.modalPopUp }}>
+        <button
+          onClick={closeModal}
+          style={{
+            position: "absolute",
+            top: "3%",
+            left: "87%",
+            background: "transparent",
+            border: "0px ",
+            fontSize: "1.2em",
+          }}
+        >
+          X
+        </button>
+        <Card
+          component="div"
+          sx={{
+            maxWidth: "100%",
+            height: "100%",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+          }}
+        >
           <CardHeader
             title={listingModal.title}
             subheader={`Post By: ${listingModal.author}`}
           />
-         <Slider {...settings}>
-                        {/* First we check if the array 'images' is empty, if it is, we use the default hardcoded image */}
-                        {listingModal.images.length > 0 ? (
-                          listingModal.images.map((url, index) => (
-                            <div key={index}>
-                              <img
-                                src={url}
-                                alt={`slide-${index}`}
-                                style={{ width: "100%", }}
-                              />
-                            </div>
-                          ))
-                        ) : (
-                          <div>
-                            <img
-                              src={image}
-                              alt="Default slide"
-                              style={{ width: "100%" }}
-                            />
-                          </div>
-                        )}
-                      </Slider>
+          <Slider {...settings} style={{ width: "100%", height:"100%" }}>
+            {/* First we check if the array 'images' is empty, if it is, we use the default hardcoded image */}
+            {listingModal.images.length > 0 ? (
+              listingModal.images.map((url, index) => (
+                <div
+                  key={index}
+                  style={{
+                    width: "100%"
+                  }}
+                >
+                  <img
+                    src={url}
+                    alt={`slide-${index}`}
+                    style={{
+                      maxHeight: "484px",
+                      marginLeft: "auto",
+                      marginRight: "auto",
+                      maxWidth: "100%",
+                    }}
+                  />
+                </div>
+              ))
+            ) : (
+              <div>
+                <img
+                  src={image}
+                  alt="Default slide"
+                  style={{ width: "100%" }}
+                />
+              </div>
+            )}
+          </Slider>
           <CardContent component="div">
             <Typography component="span" gutterBottom variant="h5">
               Date Of Event: {listingModal.dateOfSale}
