bcbc8d6eefa2040083615dceed5d19691fa30c1a
Omar4589 <omar4589@hotmail.com>
2023-10-04 00:17:02 -0500

    refactors edit listing page, updates style

---
+++ client/src/pages/EditListing/styles.js
@@ -0,0 +1,25 @@
+const styles = {
+  mainContainer: {
+    background: "#e8f5e9",
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingBottom: 10,
+    "@media (min-width: 768px)": { maxWidth: "100%" },
+  },
+  editListingContainer: {
+    width:"75%",
+    background: "white",
+    px: 4,
+    borderRadius: 2,
+    "@media (min-width: 768px)": { width:"30%" },
+  },
+  heading: { fontWeight: "bold", fontSize: 25,mt:3, mb: 1, py:2, px:2 },
+  inputBoxes: { py: 1 },
+  labels: { fontWeight: "semibold", mb: 1, display: "block" },
+  button: { bgcolor: "#66bb6a", color: "white", fontWeight: "semibold", py: 1, mb:5  },
+  
+};
+
+export default styles;
