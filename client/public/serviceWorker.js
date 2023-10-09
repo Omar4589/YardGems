332b82a77dfa81d48cdf53b7216e1d94cd4701c0
Omar4589 <omar4589@hotmail.com>
2023-10-09 00:38:52 -0500

    implements basic service worker

--- client/public/serviceWorker.js
+++ client/public/serviceWorker.js
@@ -1,25 +1,25 @@
-// // Install event - cache files
-// self.addEventListener('install', function(event) {
-//     event.waitUntil(
-//       caches.open('my-cache').then(function(cache) {
-//         return cache.addAll([
-//           '/',
-//           '/index.html',
-//           '/manifest.json',
-//           '/favicon.ico',
-//           '/whiteLogo.png',
-//           // add more assets to cache if needed
-//         ]);
-//       })
-//     );
-//   });
+// Install event - cache files
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open('my-cache').then(function(cache) {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/manifest.json',
+          '/favicon.ico',
+          '/whiteLogo.png',
+          // add more assets to cache if needed
+        ]);
+      })
+    );
+  });
   
-//   // Fetch event - fetch or fallback to cache
-//   self.addEventListener('fetch', function(event) {
-//     event.respondWith(
-//       caches.match(event.request).then(function(response) {
-//         return response || fetch(event.request);
-//       })
-//     );
-//   });
+  // Fetch event - fetch or fallback to cache
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  });
   
--- client/src/index.js
+++ client/src/index.js
@@ -3,15 +3,15 @@ import ReactDom from "react-dom";
 import App from "./App";
 
 // At the top of your index.js
-// if ('serviceWorker' in navigator) {
-//     navigator.serviceWorker.register('/serviceWorker.js')
-//       .then((registration) => {
-//         console.log('Service Worker registered with scope:', registration.scope);
-//       })
-//       .catch((error) => {
-//         console.log('Service Worker registration failed:', error);
-//       });
-//   }
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/serviceWorker.js')
+      .then((registration) => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch((error) => {
+        console.log('Service Worker registration failed:', error);
+      });
+  }
   
 
 
