6a33de9ab7439c2509a41bc8db79858b8fa89a8e
Omar4589 <omar4589@hotmail.com>
2023-10-10 18:14:54 -0500

    updates service worker to dynamically cache requests

--- client/public/serviceWorker.js
+++ client/public/serviceWorker.js
@@ -1,27 +1,34 @@
 // Last Updated: 2023-10-10T19:30:58.084Z
 
 
-// Install event - cache files
-self.addEventListener("install", function (event) {
-  event.waitUntil(
-    caches.open("my-cache").then(function (cache) {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/manifest.json",
-        "/favicon.ico",
-        "/whiteLogo.png",
-        // add more assets to cache if needed
-      ]);
-    })
-  );
-});
-
 // Fetch event - fetch or fallback to cache
 self.addEventListener("fetch", function (event) {
   event.respondWith(
     caches.match(event.request).then(function (response) {
-      return response || fetch(event.request);
+      // Cache hit - return the response from the cached version
+      if (response) {
+        return response;
+      }
+
+      // Not in cache - return the result of a call to fetch
+      return fetch(event.request).then(function (response) {
+        // Check if we received a valid response
+        if (!response || response.status !== 200 || response.type !== "basic") {
+          return response;
+        }
+
+        // Clone the response. A response is a stream
+        // and because we want the browser to consume the response
+        // as well as the cache consuming the response, we need
+        // to clone it so we have two streams.
+        var responseToCache = response.clone();
+
+        caches.open("my-cache").then(function (cache) {
+          cache.put(event.request, responseToCache);
+        });
+
+        return response;
+      });
     })
   );
 });
