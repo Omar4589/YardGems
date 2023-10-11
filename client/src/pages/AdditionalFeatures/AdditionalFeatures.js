73c038797c464ce505c996ead58745aa221f2767
Omar4589 <omar4589@hotmail.com>
2023-10-11 16:37:45 -0500

    updates imports in AdditionalFeatures

--- client/src/pages/AdditionalFeatures/AdditionalFeatures.js
+++ client/src/pages/AdditionalFeatures/AdditionalFeatures.js
@@ -2,8 +2,7 @@ import React from "react";
 import { Box, Typography } from "@mui/material";
 import styles from "./styles";
 import iphone1 from "../../assets/images/iphone1.png";
-import iphone2 from "../../assets/images/iphone2.png"
-
+import iphone2 from "../../assets/images/iphone2.png";
 
 //This is the AdditionalFeatures page is displayed if a user clicks on "MyListings","SavedListings","MyAccount" and they are not logged in.
 const AdditionalFeatures = () => {
@@ -14,13 +13,13 @@ const AdditionalFeatures = () => {
       </Typography>
 
       <Box sx={{ ...styles.featuresContainer }}>
-        <Box  sx={{ ...styles.feature }}>
+        <Box sx={{ ...styles.feature }}>
           <img src={iphone1} alt="featureImage" style={styles.img} />
           <Typography sx={{ ...styles.typography }}>
             Create, View, and Manage your listings
           </Typography>
         </Box>
-        <Box  sx={{ ...styles.feature }}>
+        <Box sx={{ ...styles.feature }}>
           <img src={iphone2} alt="featureImage" />
           <Typography sx={{ ...styles.typography }}>
             See a listing you want to keep an eye on? Save it to view it under
